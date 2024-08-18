73e5b4215993df622f0fb57f144f432996851b25
Jagath Sajjan <jagathsajjan097@gmail.com>
2024-08-18 11:15:05 +0530

    Create coin-flip.js

---
+++ commands/coin-flip.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('coin-flip')
+        .setDescription('Flip a coin'),
+    async execute(interaction) {
+        const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
+        
+        const embed = new EmbedBuilder()
+            .setTitle('Coin Flip Result')
+            .setDescription(`The coin landed on: **${result}**`)
+            .setColor('Random')
+            .setFooter({ text: 'Made By Jagath🩵' });
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
