4ed1f2dcf56ebebb0ae81a2d917e8a9f21fe432f
Jagath Sajjan <jagathsajjan097@gmail.com>
2024-08-19 22:29:17 +0530

    Update fact.js

--- commands/fact.js
+++ commands/fact.js
@@ -14,7 +14,7 @@ module.exports = {
                 .setTitle('🥶 Random Fact')
                 .setDescription(fact)
                 .setColor('Random')
-                .setFooter({ text: 'Made By Jagath🩵' });
+                .setFooter({ text: 'HackSaw Fact API.', iconURL: interaction.client.user.displayAvatarURL() });
 
             await interaction.editReply({ embeds: [embed] });
         } catch (error) {
