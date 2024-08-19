ab00759ecdd9a7831c081d41313b99e951541986
Jagath Sajjan <jagathsajjan097@gmail.com>
2024-08-19 21:16:58 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -31,16 +31,44 @@ module.exports = {
             await interaction.guild.members.ban(userToBan, { reason });
 
             const embed = new EmbedBuilder()
-                .setTitle('User Banned')
+                .setTitle('🔨 User Banned')
+                .setDescription(`${userToBan.tag} has been banned from the server.`)
                 .addFields(
-                    { name: 'Banned User', value: userToBan.tag },
-                    { name: 'Banned By', value: interaction.user.tag },
+                    { name: 'Banned User', value: userToBan.tag, inline: true },
+                    { name: 'Banned By', value: interaction.user.tag, inline: true },
                     { name: 'Reason', value: reason }
                 )
-                .setColor('Red')
-                .setFooter({ text: 'Made By Jagath🩵' });
+                .setColor('#FF0000')
+                .setThumbnail(userToBan.displayAvatarURL({ dynamic: true }))
+                .setImage('https://media.tenor.com/ZVbWtHIhQGcAAAAC/bane-no.gif')
+                .setTimestamp()
+                .setFooter({ text: 'Made By Jagath🩵', iconURL: interaction.client.user.displayAvatarURL() });
 
-            await interaction.reply({ embeds: [embed] });
+            const reply = await interaction.reply({ embeds: [embed], fetchReply: true });
+
+            // Delete the embed after 20 seconds
+            setTimeout(() => {
+                reply.delete().catch(console.error);
+            }, 20000);
+
+            // Try to send a DM to the banned user
+            try {
+                const dmEmbed = new EmbedBuilder()
+                    .setTitle('You have been banned')
+                    .setDescription(`You have been banned from ${interaction.guild.name}`)
+                    .addFields(
+                        { name: 'Reason', value: reason }
+                    )
+                    .setColor('#FF0000')
+                    .setImage('https://media.tenor.com/ZVbWtHIhQGcAAAAC/bane-no.gif')
+                    .setTimestamp()
+                    .setFooter({ text: 'Made By Jagath🩵', iconURL: interaction.client.user.displayAvatarURL() });
+
+                await userToBan.send({ embeds: [dmEmbed] });
+            } catch (error) {
+                console.log(`Could not send DM to ${userToBan.tag}`);
+            }
+
         } catch (error) {
             console.error('Error banning user:', error);
             await interaction.reply({ content: 'An error occurred while trying to ban the user.', ephemeral: true });
