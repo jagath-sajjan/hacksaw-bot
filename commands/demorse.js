52c6b6022badab2b3b5ec9254938faf1c75a4864
Jagath Sajjan <jagathsajjan097@gmail.com>
2024-08-19 22:27:21 +0530

    Update demorse.js

--- commands/demorse.js
+++ commands/demorse.js
@@ -22,7 +22,7 @@ module.exports = {
                 { name: 'Decoded Text', value: decodedText || 'Unable to decode' }
             )
             .setColor('Purple')
-            .setFooter({ text: 'Made By Jagath🩵' });
+            .setFooter({ text: 'HackSaw Demorse API.', iconURL: interaction.client.user.displayAvatarURL() });
 
         await interaction.reply({ embeds: [embed] });
     },
