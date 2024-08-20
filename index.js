4256d84901a3293c941a83be1f55f33b9f309c48
Jagath Sajjan <jagathsajjan097@gmail.com>
2024-08-20 20:33:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -126,9 +126,6 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-const express = require('express');
-const path = require('path');
-
 const app = express();
 const port = process.env.PORT || 8080;
 
